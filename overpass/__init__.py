5bd44aa118ff65bb49bee890796cf5ea005fc36b
GOATS2K <jp@glattetre.com>
2020-11-25 01:48:45 +0000

    feat(db): add standard flask setup for sqlite db

--- overpass/__init__.py
+++ overpass/__init__.py
@@ -6,6 +6,7 @@ from overpass.secrets import (
 )
 from flask_discord import DiscordOAuth2Session
 import os
+from overpass.db import init_app, close_db, init_db_command
 
 discord = DiscordOAuth2Session()
 
@@ -13,13 +14,20 @@ discord = DiscordOAuth2Session()
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.urandom(16)
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+    app.config["DATABASE"] = "overpass.db"
     app.config["DISCORD_CLIENT_ID"] = DISCORD_CLIENT_ID
     app.config["DISCORD_CLIENT_SECRET"] = DISCORD_CLIENT_SECRET
     app.config["DISCORD_REDIRECT_URI"] = DISCORD_REDIRECT_URI
 
     discord.init_app(app)
+    init_app(app)
 
     with app.app_context():
+        # Imports are done here to prevent circular import errors when
+        # importing extensions from this file
         from overpass.auth import auth
 
         app.register_blueprint(auth)
---
+++ overpass/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user (
+    id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    profile_pic TEXT NOT NULL
+)
