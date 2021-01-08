06e45b613e5de08cb62387ead9128ec1a7ac9ea3
GOATS2K <jp@glattetre.com>
2021-01-08 00:36:58 +0000

    refactor(db): add flask helper util functions

--- overpass/db.py
+++ overpass/db.py
@@ -25,6 +25,13 @@ def close_db(e=None):
         db.close()
 
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
 def init_db():
     db = get_db()
 
