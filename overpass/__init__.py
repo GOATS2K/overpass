bac973376ed9e6e3d212e7bc6a21109db924522c
GOATS2K <jp@glattetre.com>
2021-01-12 17:55:44 +0000

    refactor(flask config): load secrets from user environemnt via .env file

--- overpass/__init__.py
+++ overpass/__init__.py
@@ -1,32 +1,35 @@
 from flask import Flask
-from overpass.secrets import (
-    DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET,
-    DISCORD_REDIRECT_URI,
-)
 from flask_discord import DiscordOAuth2Session
-from flask_login import LoginManager
 import os
 from overpass.db import init_app, close_db, init_db_command
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
+
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 discord = DiscordOAuth2Session()
-login = LoginManager()
 
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = os.urandom(16)
+    secret_key = os.environ.get("OVERPASS_SECRET_KEY")
+
+    try:
+        app.secret_key = secret_key.encode()
+    except NoneType:
+        raise ValueError("The environment variable OVERPASS_SECRET_KEY is not set!")
+
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
 
     app.config["DATABASE"] = "overpass.db"
-    app.config["DISCORD_CLIENT_ID"] = DISCORD_CLIENT_ID
-    app.config["DISCORD_CLIENT_SECRET"] = DISCORD_CLIENT_SECRET
-    app.config["DISCORD_REDIRECT_URI"] = DISCORD_REDIRECT_URI
+    app.config["DISCORD_CLIENT_ID"] = os.environ.get("DISCORD_CLIENT_ID")
+    app.config["DISCORD_CLIENT_SECRET"] = os.environ.get("DISCORD_CLIENT_SECRET")
+    app.config["DISCORD_REDIRECT_URI"] = os.environ.get("DISCORD_REDIRECT_URI")
 
     discord.init_app(app)
-    login.init_app(app)
     init_app(app)
 
     with app.app_context():
