790012db190dc34812017c8ee27329bdeb8601e7
GOATS2K <jp@glattetre.com>
2021-04-17 14:03:33 +0000

    refactor(auth): document functions and remove unused endpoint

--- overpass/auth.py
+++ overpass/auth.py
@@ -1,8 +1,10 @@
 # flake8: noqa E501
 
+from typing import Any, Text, Union
 from flask import redirect, url_for, Blueprint, abort, session, current_app
 from flask.json import jsonify
 from flask.templating import render_template
+from flask.wrappers import Response
 
 # from flask import current_app as app
 from flask_discord import requires_authorization, Unauthorized, RateLimited
@@ -11,21 +13,33 @@ from overpass.db import get_db
 from datetime import datetime
 import os
 
-DISCORD_GUILD_ID = os.environ.get("DISCORD_GUILD_ID") or None
+DISCORD_GUILD_ID = os.environ.get("DISCORD_GUILD_ID") or ""
 
 auth = Blueprint("auth", __name__)
 
 
-def verify():
-    """ Verifies if user exists in the Discord guild """
+def verify() -> bool:
+    """Verifies if user exists in the Discord guild
+
+    Returns:
+        bool: User exists in guild
+    """
     guilds = discord.fetch_guilds()
     user_is_in_guild = next((i for i in guilds if i.id == int(DISCORD_GUILD_ID)), False)
     if user_is_in_guild:
         return True
+    else:
+        return False
 
 
-def add_user(username, snowflake, avatar):
-    """ Adds user to database """
+def add_user(username: str, snowflake: int, avatar: Union[str, None]) -> None:
+    """Adds user to database
+
+    Args:
+        username (str): User's username
+        snowflake (int): User's account ID
+        avatar (Union[str, None]): User's avatar URL
+    """
     current_date = datetime.now()
     db = get_db()
     current_app.logger.info(f"Adding user {username} to User table")
@@ -36,17 +50,31 @@ def add_user(username, snowflake, avatar):
     db.commit()
 
 
-def check_if_user_exists(snowflake):
-    """ Returns True if user exists in database """
+def check_if_user_exists(snowflake: int) -> bool:
+    """Returns True if user exists in database
+
+    Args:
+        snowflake (int): User's ID
+
+    Returns:
+        bool: User exists in database
+    """
     db = get_db()
     q = db.execute("SELECT * FROM user WHERE snowflake = ?", (snowflake,))
     result = q.fetchone()
 
     if result:
         return True
+    else:
+        return False
 
 
-def update_login_time(snowflake):
+def update_login_time(snowflake: int) -> None:
+    """Update the user's last logged in time
+
+    Args:
+        snowflake (int): User's ID
+    """
     current_date = datetime.now()
     db = get_db()
     db.execute(
@@ -57,18 +85,33 @@ def update_login_time(snowflake):
 
 
 @auth.route("/login/")
-def login():
+def login() -> Any:
+    """Redirect user to Discord's SSO page
+
+    Returns:
+        Any: Redirection to Discord's SSO page
+    """
     return discord.create_session(scope=["identify", "guilds"])
 
 
 @auth.route("/logout/")
-def logout():
+def logout() -> Text:
+    """Revoke a user's session
+
+    Returns:
+        Text: Flask rendered template
+    """
     discord.revoke()
     return render_template("alert.html", info="You've been logged out.")
 
 
 @auth.route("/callback/")
-def callback():
+def callback() -> Any:
+    """Callback endpoint for SSO service
+
+    Returns:
+        Any: Redirect to home page if everything went well, else return 401.
+    """
     try:
         discord.callback()
         if DISCORD_GUILD_ID:
@@ -94,13 +137,13 @@ def callback():
 
 
 @auth.errorhandler(Unauthorized)
-def redirect_discord_unauthorized(e):
+def redirect_discord_unauthorized(e) -> Any:
     return redirect(url_for("auth.login"))
 
 
 # Runs when abort(401) is called.
 @auth.errorhandler(401)
-def redirect_unauthorized(e):
+def redirect_unauthorized(e) -> Any:
     return (
         jsonify(
             {"message": "Your Discord user is not authorized to use this application."}
@@ -108,16 +151,3 @@ def redirect_unauthorized(e):
         401,
     )
 
-
-@auth.route("/me/")
-@requires_authorization
-def me():
-    resp = discord.fetch_user()
-    user = {
-        "name": resp.username,
-        "email": resp.email,
-        "id": resp.id,
-        "avatar": resp.avatar_url,
-        "verified": resp.verified,
-    }
-    return jsonify(user)
