81e378bc20daff133f66f75b2fa03d4eb1a4f7fa
GOATS2K <jp@glattetre.com>
2021-04-17 14:05:10 +0000

    refactor(archive): document functions and use more accurate variable names

--- overpass/archive.py
+++ overpass/archive.py
@@ -1,63 +1,75 @@
+from typing import Any, Dict, List, Text
 from flask import (
     Blueprint,
     send_from_directory,
     current_app,
     redirect,
     url_for,
-    request,
     abort,
 )
 from flask.templating import render_template
-from overpass.db import query_db, get_db
+from flask.wrappers import Response
+from overpass.db import get_db, query_many, query_one
 from os import environ
 from pathlib import Path
 from overpass.stream_utils import get_username_from_snowflake
 from flask_discord import Unauthorized, requires_authorization
 from overpass import discord
-from datetime import datetime
 
 
 bp = Blueprint("archive", __name__)
 
 
 @bp.errorhandler(Unauthorized)
-def redirect_discord_unauthorized(e):
+def redirect_discord_unauthorized(e) -> Any:
     return redirect(url_for("auth.login"))
 
 
 @bp.before_request
 @requires_authorization
-def require_auth():
+def require_auth() -> None:
     pass
 
 
-def archive_stream(stream_key, private=False):
-    stream_path = Path(environ.get("REC_PATH")) / f"{stream_key}.mp4"
+def archive_stream(stream_key: str):
+    """Archives a stream.
+
+    Args:
+        stream_key (str): The stream's stream key.
+    """
+    stream_path = Path(environ.get("REC_PATH", "")) / f"{stream_key}.mp4"
     current_app.logger.info(f"Adding stream {stream_path} to archive")
     db = get_db()
     db.execute(
         "UPDATE stream SET archived_file = ? WHERE stream_key = ?",
         (str(stream_path), stream_key),
     )
-    if private:
-        db.execute(
-            "UPDATE stream SET archived_file = ? WHERE stream_key = ?",
-            (str(stream_path), stream_key),
-        )
     db.commit()
 
 
-def get_archived_streams(all_metadata=False, private=False):
+def get_archived_streams(
+    all_metadata: bool = False, private: bool = False
+) -> List[Dict[str, Any]]:
+    """Get a list of all archived streams.
+
+    Args:
+        all_metadata (bool, optional): Get all metadata for stream. Defaults to False.
+        private (bool, optional): Include streams that aren't publically archived.
+        Defaults to False.
+
+    Returns:
+        List[Dict[str, Any]]: List of dicts containing the streams.
+    """
     if all_metadata:
         items = "*"
     else:
-        items = "id, user_snowflake, start_date, end_date, title, description, category, unique_id"
+        items = "id, user_snowflake, start_date, end_date, title, description, category, unique_id"  # noqa: E501
 
     if private:
-        res = query_db(f"SELECT {items} FROM stream WHERE archived_file IS NOT NULL")
+        res = query_many(f"SELECT {items} FROM stream WHERE archived_file IS NOT NULL")
     else:
-        res = query_db(
-            f"SELECT {items} FROM stream WHERE unlisted = 0 AND archivable = 1 AND archived_file IS NOT NULL"
+        res = query_many(
+            f"SELECT {items} FROM stream WHERE unlisted = 0 AND archivable = 1 AND archived_file IS NOT NULL"  # noqa: E501
         )
 
     for stream in res:
@@ -71,19 +83,35 @@ def get_archived_streams(all_metadata=False, private=False):
     return res
 
 
-def serve_file(res):
-    username = get_username_from_snowflake(res["user_snowflake"])
-    filename = f"{username} - {res['title']} - {res['start_date']}.mp4"
+def serve_file(archived_stream: Dict[str, Any]) -> Response:
+    """Serves an archived stream from storage.
+
+    Args:
+        archived_stream (Dict[str, Any]):
+        Dict containing information about the stream from the database.
+
+    Returns:
+        Response: File being served by Flask's Response class.
+    """
+    username = get_username_from_snowflake(archived_stream["user_snowflake"])
+    filename = (
+        f"{username} - {archived_stream['title']} - {archived_stream['start_date']}.mp4"
+    )
     return send_from_directory(
         environ.get("REC_PATH"),
-        filename=f"{res['stream_key']}.mp4",
+        filename=f"{archived_stream['stream_key']}.mp4",
         as_attachment=True,
         attachment_filename=filename,
     )
 
 
 @bp.route("/")
-def list_archived_streams():
+def list_archived_streams() -> Text:
+    """List publically archived streams.
+
+    Returns:
+        Text: Static page rendered by Flask.
+    """
     archive = get_archived_streams()
     if archive:
         return render_template("archive.html", archive=archive)
@@ -92,17 +120,27 @@ def list_archived_streams():
 
 
 @bp.route("/download/<unique_id>")
-def serve_archive(unique_id):
+def serve_archive(unique_id: str) -> Response:
+    """Serves stream from the archive.
+
+    Args:
+        unique_id (str): The stream's unique ID.
+
+    Returns:
+        Response: File being served by Flask's Response class.
+    """
     user = discord.fetch_user()
-    res = query_db(
+    stream = query_one(
         "SELECT * FROM stream WHERE archived_file IS NOT NULL AND unique_id = ?",
         [unique_id],
-        one=True,
     )
-    if user.id == res["user_snowflake"]:
-        return serve_file(res)
-    elif bool(res["archivable"]):
-        return serve_file(res)
+    if user.id == stream["user_snowflake"]:
+        # The user requesting the stream created it.
+        # Therefore they shall always have access to the file.
+        return serve_file(stream)
+    elif bool(stream["archivable"]):
+        # The stream is publically archived.
+        return serve_file(stream)
     else:
+        # The stream does not exist.
         return abort(404)
-    # Should render a 404 if the stream is unlisted
